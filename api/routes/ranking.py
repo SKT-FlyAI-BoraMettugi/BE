0f2fc0027719a6767869e4ca849e8cecfb017dcd
hanseul37 <hanseul37@daum.net>
2025-02-19 21:27:51 +0900

    [FEAT] 유저 랭킹 조회 API 구현

--- api/routes/ranking.py
+++ api/routes/ranking.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database.nolly import get_db
-from core.ranking import calculate_ranking
+from core.ranking import calculate_ranking, calculate_user_ranking
 from crud.user import get_all_users_score
 
 router = APIRouter()
@@ -11,3 +11,9 @@ async def get_all_ranking(db: Session = Depends(get_db)):
     scores = get_all_users_score(db)
     rankings = calculate_ranking(scores)
     return rankings
+
+@router.get('/{user_id}')
+async def get_ranking(user_id: int, db: Session = Depends(get_db)):
+    scores = get_all_users_score(db)
+    ranking = calculate_user_ranking(scores, user_id)
+    return ranking
--- core/ranking.py
+++ core/ranking.py
@@ -6,3 +6,9 @@ def calculate_ranking(scores: list[Score]) -> list[Ranking]:
     rankings = [Ranking(user_id=score.user_id, score=score.score, rank=idx + 1)
                 for idx, score in enumerate(sorted_scores)]
     return rankings
+
+def calculate_user_ranking(scores: list[Score], user_id) -> Ranking:
+    sorted_scores = sorted(scores, key=lambda x: x.score, reverse=True)
+    for idx, score in enumerate(sorted_scores):
+        if score.user_id == user_id:
+            return Ranking(user_id=score.user_id, score=score.score, rank=idx + 1)
