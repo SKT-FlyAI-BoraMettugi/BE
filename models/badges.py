6f8b9defbdf7c686a19cbaa8774cede91bf6af49
unknown <bokyeong0405@Khu.ac.kr>
2025-02-25 21:32:38 +0900

    [REFACTOR] 금은동 영어로 변경

--- models/badges.py
+++ models/badges.py
@@ -9,6 +9,6 @@ class Badges(Base):
     user_id = Column(BigInteger, ForeignKey("users.user_id"), autoincrement=True, nullable=False) # 사용자 번호
     theme_id = Column(BigInteger, ForeignKey("theme.theme_id"), autoincrement=True, nullable=False) # 테마 번호
 
-    grade = Column(Enum("금","은","동", name="badge_grade"), nullable=True) # 배지 등급
+    grade = Column(Enum("gold","silver","copper", name="badge_grade"), nullable=True) # 배지 등급
     created_date = Column(DateTime, server_default=func.now()) # 배지 생성 시간
     updated_date = Column(DateTime, onupdate=func.now()) # 배지 수정 시간
